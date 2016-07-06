c4294a28ea1a36c896a9e7d3049461cdc430d0d1
Condzi <koondzi12@wp.pl>
2016-07-06 22:44:42 +0200

    Added GameObject class

---
+++ Source/TechDemo/GameObject.cpp
@@ -0,0 +1,69 @@
+#include "GameObject.hpp"
+
+
+void GameObject::draw(sf::RenderTarget & target, sf::RenderStates states) const
+{
+	if (m_sprite != nullptr)
+		target.draw(*m_sprite, states);
+}
+
+GameObject::GameObject()
+{
+	m_texture = nullptr;
+	m_textureManager = nullptr;
+	m_boxCollider = nullptr;
+	m_rigidbody = nullptr;
+	m_sprite = nullptr;
+}
+
+
+GameObject::~GameObject()
+{
+	const_cast<TextureManager*>(m_textureManager)->GiveBackTexture(m_texture);
+	delete m_sprite;
+}
+
+void GameObject::SetTexture(const TextureFixed * texture)
+{
+	m_texture = texture;
+	m_sprite = new sf::Sprite;
+	ChangeSpriteAttributes()->setTexture(*m_texture);
+}
+
+void GameObject::SetTextureManagerPointer(const TextureManager * textureManager)
+{
+	m_textureManager = textureManager;
+}
+
+void GameObject::SetBoxCollider(BoxCollider * collider)
+{
+	m_boxCollider = collider;
+}
+
+void GameObject::SetRigidbody(Rigidbody * rigidbody)
+{
+	m_rigidbody = rigidbody;
+}
+
+sf::Sprite * GameObject::ChangeSpriteAttributes()
+{
+	if (m_sprite == nullptr)
+		return nullptr;
+
+	return const_cast<sf::Sprite*>(m_sprite);
+}
+
+TextureFixed GameObject::GetTexture()
+{
+	return *m_texture;
+}
+
+BoxCollider GameObject::GetBoxCollider()
+{
+	return *m_boxCollider;
+}
+
+Rigidbody GameObject::GetRigidbody()
+{
+	return *m_rigidbody;
+}
---
+++ Source/TechDemo/GameObject.hpp
@@ -0,0 +1,40 @@
+#pragma once
+#include <SFML\Graphics.hpp>
+
+#include "..\Graphic\TextureManager\TextureManager.hpp"
+#include "..\Physic\Colliders\BoxCollider.hpp"
+#include "..\Physic\Rigidbody\Rigidbody.hpp"
+
+
+class GameObject :
+	public sf::Drawable
+{
+private:
+	void draw(sf::RenderTarget & target, sf::RenderStates states) const;
+
+public:
+	GameObject();
+	~GameObject();
+
+	void SetTexture(const TextureFixed * texture);
+	void SetTextureManagerPointer(const TextureManager * textureManager);
+	void SetBoxCollider(BoxCollider * collider);
+	void SetRigidbody(Rigidbody * rigidbody);
+
+	sf::Sprite * ChangeSpriteAttributes();
+
+	TextureFixed GetTexture();
+	BoxCollider GetBoxCollider();
+	Rigidbody GetRigidbody();
+	
+	virtual void Update(sf::Event * eventPtr = nullptr) = 0;
+
+private:
+	const TextureFixed * m_texture;
+	const TextureManager * m_textureManager;
+	BoxCollider * m_boxCollider;
+	Rigidbody * m_rigidbody;
+	const sf::Sprite * m_sprite;
+	
+};
+
